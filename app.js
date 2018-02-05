040223c91d1e63c2957398ca02bfaebebed321f2
yuelight <vaeqfe@gmail.com>
2018-02-05 16:22:21 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const { readFile, rename } = require('fs');
+const { rename } = require('fs');
 const { promisify } = require('util');
 const { resolve } = require('path');
 
-const readFileSync = async (oldPath, newPath) => {
-	const arr = [];
-
-	await promisify(rename)(oldPath, newPath);
-	let data = await promisify(readFile)(newPath, 'utf8');
-	data = JSON.parse(data);
-
-	for (const v in data.devDependencies) {
-		arr.push(v);
-	}
-	console.log(data.scripts);
-	console.log(arr.join(' '));
-};
+const readFileSync = async (oldPath, newPath) => await promisify(rename)(oldPath, newPath);
 
 readFileSync(resolve('package.json'), resolve('package-dev.json'));
